fb0f55b968629259a941f136451b8037f2cb4ee2
lshui1021 <iceloveq1021@gmail.com>
2025-12-04 16:36:02 +0800

    prepare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/SmartEntry/', // <--- 這裡設定子路徑
     plugins: [react()],
     server: {
         proxy: {
